17a13a56b71f3880888b3623816c1bd21f8166c3
alireza olama <alireza.lm69@gmail.com>
2022-01-03 12:22:22 +0330

    minor

--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@
 echo "Running Server..."
 
 # make sure dependencies are installed and python venv is activated.
+
+
+
 pip install -r req.txt
 
 uvicorn server:app --reload --host 0.0.0.0 --port 8000
