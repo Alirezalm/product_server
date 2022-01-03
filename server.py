04de35781e46e84262b2bead8fbcda017e6e465f
alireza olama <alireza.lm69@gmail.com>
2022-01-03 11:33:26 +0330

    server created

--- server.py
+++ server.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class Product(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: str
 
 
 @app.get("/")
 def main():
-    return {"status": "product successfully recieved."}
+    print("hi")
+    return {"status": "API Online."}
+
+@app.post("/products/")
+def add_product(product: Product):
+    print(product)
+    return {"status": "product added successfully."}
